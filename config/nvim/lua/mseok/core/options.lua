d02357a07218707c7f42daf011193400e4a112ac
mseok <mshmjp02@gmail.com>
2025-05-21 22:20:09 +0900

    chore: update osc

--- config/nvim/lua/mseok/core/options.lua
+++ config/nvim/lua/mseok/core/options.lua
@@ -49,16 +49,23 @@ vim.keymap.set(
 	{ desc = "yank to system clipboard in visual mode. You can combinate this like Vjj<leadyer>y." }
 )
 
+local function paste()
+  return {
+    vim.fn.split(vim.fn.getreg(""), "\n"),
+    vim.fn.getregtype(""),
+  }
+end
+
 vim.g.clipboard = {
-	name = "OSC 52",
-	copy = {
-		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-	},
-	paste = {
-		["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-		["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-	},
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = paste,
+    ["*"] = paste,
+  },
 }
 
 -- statusline
